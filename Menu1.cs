ab023aec4489179bd197eac490051bc94afa5b2a
Wojciech30 <o123.123@o2.pl>
2023-10-24 18:39:21 +0200

    Add menu

---
+++ Menu1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serwis_Aut
+{
+    public class Printer : IPrinter
+    {
+        public string Print(string msg)
+        {
+            Console.WriteLine(msg);
+            return msg;
+        }
+    }
+    public class Menu1
+    {
+        public void Menu()
+        {
+            var printer = new Printer();
+
+            while (true)
+            {
+                Console.Clear();
+                var res = Console.ReadLine();
+                switch (res)
+                {
+                    case "1":
+                        {
+                            printer.Print("Ok");
+                            break;
+                        }
+                    case "2":
+                        {
+                            printer.Print("Not ok");
+                            break;
+                        }
+                    case "X":
+                        {
+                            printer.Print("Do widzenia");
+                            return;
+                        }
+                    default:
+                        {
+                            printer.Print("Nie ma takiej opcji");
+                            break;
+                        }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+    public interface IPrinter
+    {
+        public string Print(string msg);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,15 +4,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        CarService carService = new CarService();
-        
-        Car car = new Car(1, "Mercedes", "Yellow");
-        Car car1 = new Car(0, "Mercedes B5", "Yellow");
-        Car car2 = new Car(3, "Mercedes", "Yellow1");
-        
-        carService.InsertNewCar(car.CarId,car.Mark,car.Color);
-        carService.InsertNewCar(car2.CarId,car2.Mark,car2.Color);
-        carService.InsertNewCar(car1.CarId,car1.Mark,car1.Color);
+        var menu = new Menu1();
+        menu.Menu();
 
     }
 }
